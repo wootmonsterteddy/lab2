d15e768ca3100a2dee5e9de6569535ac3f4fefac
wootmonsterteddy <32763526+wootmonsterteddy@users.noreply.github.com>
2017-10-16 14:13:22 +0200

    a

--- lab2_funcs.c
+++ lab2_funcs.c
@@ -13,30 +13,8 @@ struct arr A,B,C,R,X,Y;
 
 void readInput(void)
 {
-	struct commandList
-	{
-		char* name;
-		int n;
-	};
-	struct commandList commands[100] =
-	{
-			{"help",		1},
-			{"set",			2},
-			{"sin",			3},
-			{"show",		4},
-			{"showFile",	5},
-			{"deleteFile",	6},
-			{"importCSV",	7},
-			{"exportCSV",	8},
-			{"exportMAT",	9},
-			{"exportJSON", 	10},
-			{"exportXML", 	11},
-			{"quit",		12},
-			{"exit",		13},
-			{"exit x",	 	14},
-	};
-	char* input[50];
 
+	char* input[50];
 	fgets(input,50,stdin);
 
 }
